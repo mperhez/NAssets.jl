67ae8583d6c13cf29c9f530c9300e6eb85ea2f18
mperhez <marco.perez@ieee.org>
2021-07-20 14:43:04 +0100

    adding geo, network, plotting files

--- src/netManAbm.jl
+++ src/netManAbm.jl
@@ -1,6 +1,5 @@
 # time-to-event functions
 include("model/tte_functions.jl")
-
 #Core structures
 include("model/netManStructs.jl")
 include("model/of_switch.jl")
@@ -9,8 +8,14 @@ include("model/agent_control.jl")
 
 include("model/physical_model.jl")
 
+include("model/network_model.jl")
+include("model/geo_model.jl")
+
 #Main Functions
 include("model/netManFunctions.jl")
+
+#Plotting functions
+include("utils/plotting_functions.jl")
 
 #Agents.jl function implementation for this model
 include("model/netManModel.jl")
