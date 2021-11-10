1ddec14bee2455db30dcd28b9e74a17306046375
mperhez <marco.perez@ieee.org>
2021-11-10 23:41:33 +0000

    refactor functions to corresponding files

--- src/experiments_from_csv.jl
+++ src/experiments_from_csv.jl
@@ -70,83 +70,21 @@ using Distributed
 export load_run_configs, single_run_with_logging
 
 
-function find_paths_by_seed(seed,g::G)where G<: AbstractGraph
-    Random.seed!(seed)
-
-    cc = closeness_centrality(g)
-    cci = sort([ (i,cc[i]) for i=1:length(cc) ],by=last,rev=true)
-
-    pending = cci 
-    cp =[]
-    coverage = 0.95
-
-        while length(pending) >= (1- coverage) * nv(g)
-            pending_i = [ first(p) for p in pending]
-            
-            #node with the most closeness_centrality
-            s = first(first(pending))
-
-            #max distance to any other pending node
-            ds = gdistances(g,s)
-            sds = sort([(i,ds[i]) for i=1:length(ds) if i in pending_i ],by=last,rev=true)
-            d = first(first(sds))
-            
-            #shortest path between these two nodes
-            sp = first(yen_k_shortest_paths(g,s,d).paths)
-            push!(cp, sp)
-
-            #remove nodes in the shortest path from pending list
-            pending_i = collect(setdiff([ first(p) for p in pending ],Set(sp)))
-            # println(pending_i)
-            pending = [ p for p in pending  if first(p) in pending_i ]
-        end
-    return cp
-end
-
-function find_services(seed,g::G)where G<:AbstractGraph
-    return [ (first(p),last(p)) for p in find_paths_by_seed(seed,g) ]
-end
-
-function create_csv_template(file_name,config)
-    open(file_name * ".csv","w") do io
-        writedlm(io,vcat(reshape(collect(keys(config)),1,length(config)),reshape(collect(values(config)),1,length(config))),";")
-    end
-end
-
-function load_base_cfgs(filename)
-    df_c = CSV.File(filename,types=Dict(:deterioration => Float64)) |> DataFrame
-    base_cfgs = []
-    for row in eachrow(df_c)
-        vals = []
-        for nm in names(df_c)
-            val = @match String(nm) begin
-                "traffic_dist_params" => parse.([Float64],split(row[:traffic_dist_params][2:end-1],","))
-                _ => row[nm]
-            end
-            push!(vals,val)
-        end
-        push!(base_cfgs, (;zip(Tuple(Symbol.(names(df_c))),vals)...))
-    end
-    return base_cfgs
-end
-
 base_cfgs = load_base_cfgs("data/configs/configs.csv")
-
+#obtain services that provide this coverage of the network
+coverage = 0.95
 configs = []
 for bcfg in base_cfgs
-    ntw_topo = GraphModel(bcfg.ntw_topo_n)
-    ntw_services = find_services(bcfg.seed,get_graph(bcfg.seed,bcfg.size,ntw_topo;k=bcfg.k))
-    full_config = NamedTuple{Tuple(vcat([:ctl_model,:ntw_topo,:ntw_services],collect(keys(bcfg))))}(vcat([ GraphModel(bcfg.ctl_model_n),ntw_topo,ntw_services],collect(values(bcfg))))
-
-    push!(configs,full_config)
+    ntw_services = get_end_points(bcfg.seed,get_graph(bcfg.seed,bcfg.size,GraphModel(bcfg.ntw_topo_n);k=bcfg.k,adj_m_csv=bcfg.ntw_csv_adj_matrix),coverage)
+    push!(configs,config(bcfg,ntw_services))
     
 end
 
 
 BenchmarkTools.DEFAULT_PARAMETERS.samples = 100
 
-# single_run_with_logging(configs[1])
+single_run_with_logging(configs[1])
 #single_run_with_file_logging(configs[1])
-pmap(single_run_with_file_logging,configs)
+#pmap(single_run_with_file_logging,configs)
 
 # end # module
